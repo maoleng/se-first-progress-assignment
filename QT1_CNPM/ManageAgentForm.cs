45c2f5d3a2a133677685fb9b240a860675708825
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-13 23:27:00 +0700

    feat: usecase manage agent

--- QT1_CNPM/ManageAgentForm.cs
+++ QT1_CNPM/ManageAgentForm.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace QT1_CNPM
 {
     public partial class ManageAgentForm : Form
     {
+        SqlConnection cn;
+        SqlDataAdapter data;
+        SqlCommand cm;
+        DataTable tb;
+
         public ManageAgentForm()
         {
             InitializeComponent();
+        }
+
+        private void ManageAgentForm_Load(object sender, EventArgs e)
+        {
+            string sql = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+            this.cn = new SqlConnection(sql);
+            this.cn.Open();
+
+            fillDataGrid("SELECT * FROM Agent");
+
+        }
+
+        void fillDataGrid(string sql)
+        {
+            this.data = new SqlDataAdapter(sql, this.cn);
+            tb = new DataTable();
+            this.data.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void btnAgentAdd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
