67b4443c137a14e3f26741f6207bd9728988ebab
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-14 00:50:01 +0700

    feat: manage order

--- QT1_CNPM/OrderForm.Designer.cs
+++ QT1_CNPM/OrderForm.Designer.cs
@@ -28,19 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.iOrderId = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOrderDetail = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Order
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Order ID";
+            // 
+            // iOrderId
+            // 
+            this.iOrderId.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iOrderId.Location = new System.Drawing.Point(159, 23);
+            this.iOrderId.Name = "iOrderId";
+            this.iOrderId.Size = new System.Drawing.Size(100, 38);
+            this.iOrderId.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 174);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(721, 264);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnOrderDetail
+            // 
+            this.btnOrderDetail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrderDetail.Location = new System.Drawing.Point(297, 20);
+            this.btnOrderDetail.Name = "btnOrderDetail";
+            this.btnOrderDetail.Size = new System.Drawing.Size(305, 41);
+            this.btnOrderDetail.TabIndex = 4;
+            this.btnOrderDetail.Text = "View Order Detail";
+            this.btnOrderDetail.UseVisualStyleBackColor = true;
+            this.btnOrderDetail.Click += new System.EventHandler(this.btnOrderDetail_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAll.Location = new System.Drawing.Point(615, 21);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(130, 40);
+            this.btnAll.TabIndex = 5;
+            this.btnAll.Text = "All";
+            this.btnAll.UseVisualStyleBackColor = true;
+            // 
+            // OrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Order";
+            this.ClientSize = new System.Drawing.Size(781, 450);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnOrderDetail);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.iOrderId);
+            this.Controls.Add(this.label1);
+            this.Name = "OrderForm";
             this.Text = "Order";
+            this.Load += new System.EventHandler(this.OrderForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox iOrderId;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOrderDetail;
+        private System.Windows.Forms.Button btnAll;
     }
 }
--- QT1_CNPM/OrderForm.cs
+++ QT1_CNPM/OrderForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,42 @@ namespace QT1_CNPM
 {
     public partial class OrderForm : Form
     {
+        SqlConnection cn;
+        SqlDataAdapter data;
+        SqlCommand cm;
+        DataTable tb;
+
         public OrderForm()
         {
             InitializeComponent();
+        }
+
+        private void btnOrderDetail_Click(object sender, EventArgs e)
+        {
+            int order_id = Int16.Parse(iOrderId.Text);
+            fillDataGrid($"SELECT \"ORDER\".orderid AS \"Order ID\", item.ItemName AS \"Item Name\", quantity, price\r\nFROM \"ORDER\" \r\nLEFT JOIN orderdetail ON orderdetail.OrderID = \"ORDER\".OrderID \r\nLEFT JOIN item ON item.itemid = orderdetail.itemid\r\nWHERE \"order\".orderid = {order_id}");
+        }
+
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+            string sql = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+            this.cn = new SqlConnection(sql);
+            this.cn.Open();
+
+            fillDataGrid("SELECT * FROM \"Order\"");
+        }
+        void fillDataGrid(string sql)
+        {
+            this.data = new SqlDataAdapter(sql, this.cn);
+            tb = new DataTable();
+            this.data.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        void executeSql(string sql)
+        {
+            SqlCommand query = new SqlCommand(sql, this.cn);
+            query.ExecuteNonQuery();
         }
     }
 }
